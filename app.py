e2d992e77ceff1eb4beebe64fbc6c9ddad0e8434
Christopher Verch <cv932@nyu.edu>
2020-04-19 16:42:39 -0400

    Added encryption and reading from data.txt

--- app.py
+++ app.py
@@ -3,19 +3,50 @@ import numpy.random
 application = Flask(__name__,template_folder='template',static_folder='static')
 from json import dumps
 from requests import post
+import time
+from cryptography.fernet import Fernet
+#change this if more rooms added
+numRooms=6
+
 @application.route('/postdata', methods=['GET','POST'])
 def create_row_in_gs():
     if request.method == 'GET':
         return "error"
     if request.method == 'POST':
-        reqId = request.json['id']
-        f= open("data.txt","w")
-        f.write(reqId)
+        key=("R4WBmuFIHoTaz9recdTsrMYETGhAYXuLXoOm-kVr2JE=".encode())
+        cipher_suite = Fernet(key)
+        print(request.json['data'])
+        
+        data = request.json['data']
+        index = request.json['index']
+        timeInt=int(time.time())
+        decodedTime = cipher_suite.decrypt(request.json['time'].encode())
+        decodedTime = decodedTime.decode("utf-8")
+        if(abs(int(decodedTime)-timeInt) < 30):
+            #if the message was received in the last 30 seconds, then
+            #write the data
+            fileData=""
+            indexNum=0
+            with open('data.txt','r') as openfileobject:
+                for line in openfileobject:
+                    if(indexNum==index):
+                        fileData+=data
+                        fileData+="\n"
+                    else:
+                        fileData+=line
+                    indexNum+=1
+            f=open("data.txt","w")
+            f.write(fileData)
+            f.close()
+        
 	
-        return reqId
+        return data
 @application.route('/')
 def hello_world():
-    populations = numpy.random.randint(low=1, high=10, size=6)
+    f = open("data.txt","r")
+    populations = [0]*numRooms
+    for x in range(numRooms):
+        populations[x] = int(f.readline())
     colors = ["green"]*6
     for i in range(len(populations)):
         if 0<=populations[i]<=5:
@@ -24,12 +55,9 @@ def hello_world():
             colors[i] = "yellow"
         else:
             colors[i] = "red"
-    f= open("data.txt","r")
-    if f.mode == 'r':
-        content =f.read().split(',')
-        content = [int(item) for item in content]
+    
     print(colors)
     return render_template("index.html",value = colors, data=populations)
 
 if __name__ == '__main__':
-    application.run()
+    application.run(port=80)
