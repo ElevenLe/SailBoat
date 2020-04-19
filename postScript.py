7a53daaeb31a4cb01c92312ffe6f1bb6c73d5e82
Christopher Verch <cv932@nyu.edu>
2020-04-19 14:35:14 -0400

    Added encryption

--- postScript.py
+++ postScript.py
@@ -1,10 +1,15 @@
+from cryptography.fernet import Fernet
 import json
 import requests
-
+import time
 def postData(data):
-    api_url = 'http://howfull.us-west-2.elasticbeanstalk.com/postdata'
-    create_row_data = {'id': str(data) }
+    key=("R4WBmuFIHoTaz9recdTsrMYETGhAYXuLXoOm-kVr2JE=".encode())
+    cipher_suite = Fernet(key)
+    timeInt=int(time.time())
+    timeEncBytes = cipher_suite.encrypt(str(timeInt).encode())
+    timeEncStr = timeEncBytes.decode("utf-8")
+    api_url = 'http://127.0.0.1/postdata'
+    create_row_data = {'data': str(data), 'time': timeEncStr }
     print(create_row_data)
     r = requests.post(url=api_url, json=create_row_data)
     print(r.status_code, r.reason, r.text)
-
